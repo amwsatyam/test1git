55918be6095a3a348d967510e64901e1ee6e9b4a
amwsatyam <amw.satyam@gmail.com>
2018-08-06 16:11:29 +0530

    newpull
    new

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+      server: {
+        baseDir: './'
+      },
+    })
+  })
+//gulp.task('sass', function() {
+ //   return gulp.src('scss/**/*.scss') 
+ //     .pipe(sass())
+  //    .pipe(gulp.dest('css'))
+  //    .pipe(browserSync.reload({
+   //     stream: true
+  //    }))
+ // });
+
+  gulp.task('watch', ['browserSync'], function (){
+    gulp.watch('css/**/*.css', browserSync.reload); 
+    gulp.watch('*.html', browserSync.reload); 
+    gulp.watch('/js/**/*.js', browserSync.reload); 
+  });
